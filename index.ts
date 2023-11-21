b99cae9d255dcbcf0842c69fa84b80a91c38645a
Okwudili-Daniel <144442872+Okwudili-Daniel@users.noreply.github.com>
2023-11-21 14:26:41 +0100

    Daniel

--- index.ts
+++ index.ts
@@ -11,6 +11,14 @@ const server = app.listen(port, () =>{
     console.log("server listening on port: ", port)
 });
 
-process.on("uncaughtException", () =>{
-    
+process.on("uncaughtException", (err: Error) =>{
+    console.log("uncaughtException", err)
+    process.exit(1);
+})
+
+process.on("unhandledRejection", (reason) =>{
+    console.log("unhandledRejection", reason)
+    server.close(() =>{
+        process.exit(1);
+    })
 })
