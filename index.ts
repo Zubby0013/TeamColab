447383af9faa6815cdd3b4da4821fa04ff9f3891
Okwudili-Daniel <144442872+Okwudili-Daniel@users.noreply.github.com>
2023-11-21 14:21:04 +0100

    Daniel

--- index.ts
+++ index.ts
@@ -2,4 +2,15 @@ import express, { Application } from "express";
 import cors from "cors"
 
 const app: Application = express();
-const port: number = 1000
+const port: number = 1000;
+
+app.use(express.json());
+app.use(cors());
+
+const server = app.listen(port, () =>{
+    console.log("server listening on port: ", port)
+});
+
+process.on("uncaughtException", () =>{
+    
+})
